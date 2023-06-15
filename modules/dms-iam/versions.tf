cfee84352de4f0d96cd6f6ebda3ebf546886e114
Max Lobur <max_lobur@outlook.com>
2023-06-15 20:48:26 +0300

    Update versions.tf

--- modules/dms-iam/versions.tf
+++ modules/dms-iam/versions.tf
@@ -5,11 +5,6 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.0"
-      # In particular:
-      # https://github.com/hashicorp/terraform-provider-aws/pull/24047
-      # https://github.com/hashicorp/terraform-provider-aws/pull/23692
-      # https://github.com/hashicorp/terraform-provider-aws/pull/13476
-      version = ">= 4.26.0"
     }
   }
 }
