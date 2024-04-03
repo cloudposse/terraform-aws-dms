f110ff11aa91ccded63f1b9f03d1ae99e9f4476a
Matt Gowie <matt@masterpoint.io>
2024-04-03 12:58:41 -0600

    feat: add subnet group id output

--- modules/dms-replication-instance/outputs.tf
+++ modules/dms-replication-instance/outputs.tf
@@ -7,3 +7,8 @@ output "replication_instance_arn" {
   value       = join("", aws_dms_replication_instance.default[*].replication_instance_arn)
   description = "Replication instance ARN"
 }
+
+output "subnet_group_id" {
+  value       = join("", aws_dms_replication_subnet_group.default[*].replication_subnet_group_id)
+  description = "Replication subnet group ID"
+}
